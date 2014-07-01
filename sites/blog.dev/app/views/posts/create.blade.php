2f40cae695869ce8658410baba459f8a7749bb89
William Martin <williammartincruz@gmail.com>
2014-07-01 11:22:24 -0500

    added errors

--- sites/blog.dev/app/views/posts/create.blade.php
+++ sites/blog.dev/app/views/posts/create.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <h1>Create new post</h1>
+    @if ($errors->has('title'))
+    	{{ $errors->first('title', '<span class="help-block">:message</span>') }}
+    @endif	
 	<form action="{{{ action('PostsController@store') }}}" method="POST">
 	<label for="title">Post Title</lable>
 	<input type="text" id="title" name="title" value="{{{ Input::old('title') }}}">
