af41545898e0d12c57ac3d8b69d1c0f96b608f8e
William Martin <williammartincruz@gmail.com>
2014-07-03 17:55:04 -0500

    added user function to post model

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,11 +1,20 @@
 <?php
 
-class Post extends Eloquent {
+class Post extends BaseModel {
 
 	// The db table this modl relates to
     protected $table = 'posts';
 
     // Validation rules for our model properties
-    static public $rules = [ 'title' => 'required|max:100' ];
+    static public $rules = [ 
+    	'title' => 'required|max:100',
+    	'body' => 'required|max:250'
 
+    ];
+
+	
+	public function user()
+		{
+		    return $this->belongsTo('User');
+		}
 }
